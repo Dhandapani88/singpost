df9de1e811a2f6c1b82aac339c7c9739a8257aac
Dhandapani <rdhandapani88@gmail.com>
2013-11-22 18:49:37 +0530

    change commit

--- Eloss/src/eloss.java
+++ Eloss/src/eloss.java
@@ -17,7 +17,7 @@ public class eloss {
   @Before
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
-    baseUrl = "https://heartqaapps.farmersinsurance.com/";
+    baseUrl = "https://heartmtdm.farmersinsurance.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
